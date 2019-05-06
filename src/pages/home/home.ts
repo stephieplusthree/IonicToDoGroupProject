6e9da1ad8bc09db17c2d358468031e75e8bd54fc
Ashwind34 <ahester34@gmail.com>
2019-05-05 21:47:04 -0700

    removed navigation from home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,24 +1,13 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import { LoginPage } from '../login/login';
-import { RegisterPage } from '../register/register';
+import { Component } from '@angular/core'
 
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor() { }
 
-  }
-  
-  goToLogin() {
-    this.navCtrl.push(LoginPage)
-  }
-
-  goToRegister() {
-    this.navCtrl.push(RegisterPage)  
-  }
 }
